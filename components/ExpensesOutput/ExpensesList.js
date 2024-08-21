637919f363d11426c526f0ff32139526f667dbc2
OlubodeMichael <moolubode3@gmail.com>
2024-08-20 23:33:43 -0400

    Added navigations and use fake data to test

---
+++ components/ExpensesOutput/ExpensesList.js
@@ -0,0 +1,19 @@
+import { FlatList, Text } from "react-native"
+import ExpenseItem from "./ExpenseItem"
+
+function renderExpenseItem(itemData) {
+    return <ExpenseItem  
+                description={itemData.item.description}
+                amount={itemData.item.amount}
+                date={itemData.item.date}
+            />
+}
+function ExpensesList({expenses}) {
+    return <FlatList 
+                data={expenses} 
+                renderItem={renderExpenseItem} 
+                keyExtractor={(item) => item.id}
+            />
+}
+
+export default ExpensesList
